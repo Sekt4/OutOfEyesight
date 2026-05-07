de14bf1e4365eeeeafd5e7266ee9513496272535
CSSredowaste <sanebolsin@gmail.com>
2026-05-07 19:25:16 +0700

    Day 15 commit

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,8 +8,6 @@ public class PlayerMovement : MonoBehaviour
     InputAction sprintAction;
     Rigidbody2D rb;
     [SerializeField] float moveSpeed = 5;
-    [SerializeField] float walkSpeed = 5;
-    [SerializeField] float sprintSpeed = 8;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,15 +23,6 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 moveVector = moveAction.ReadValue<Vector2>();
         rb.linearVelocity = moveVector * moveSpeed;
-
-        if(sprintAction.IsPressed() && moveSpeed != sprintSpeed)
-        {
-            moveSpeed = sprintSpeed;
-        }
-        else if(!sprintAction.IsPressed() && moveSpeed == sprintSpeed)
-        {
-            moveSpeed = walkSpeed;
-        }
         if(moveVector.x > 0)
         {
             Vector3 s = transform.localScale;
