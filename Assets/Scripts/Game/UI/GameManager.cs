1efe454e28d42794251874c1d1411e01d88a333a
CSSredowaste <sanebolsin@gmail.com>
2026-05-11 19:25:17 +0700

    Day 16 commit

--- Assets/Scripts/Game/UI/GameManager.cs
+++ Assets/Scripts/Game/UI/GameManager.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] Animator loadingScreenAnimator;
+    [SerializeField] Animator loadingEyeAnimator;
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
